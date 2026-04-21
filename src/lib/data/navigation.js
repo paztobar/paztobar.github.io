110d2db000832e391769de675d2903282570ba2e
Sebastian Viteri <msusvv@achs.cl>
2026-04-20 22:07:16 -0400

    Refactor code structure for improved readability and maintainability

--- src/lib/data/navigation.js
+++
@@ -1,7 +0,0 @@
-export const navigation = [
-	{ title: 'Inicio', href: '/' },
-	{ title: 'Portfolio', href: '/portfolio' },
-	{ title: 'Blog', href: '/blog' },
-	{ title: 'Lab', href: '/lab' },
-	{ title: 'Contacto', href: '/contacto' }
-];
